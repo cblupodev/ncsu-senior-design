8cc73e72e1133962c12d430e79b7c28ecef82357
Christopher Lupo <chrisblupo@gmail.com>
2017-01-26 13:44:42 -0500

    -

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using Microsoft.CodeAnalysis;
+//using Microsoft.CodeAnalysis.CSharp;
+//using Microsoft.CodeAnalysis.CSharp.Symbols;
+//using Microsoft.CodeAnalysis.CSharp.Syntax;
+//using Microsoft.CodeAnalysis.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("asdf");
+            }
+        }
+    }
+}
