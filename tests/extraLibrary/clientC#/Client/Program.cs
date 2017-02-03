8ccf011409a03f6b6a89c79f790ecc61038654f7
nafaulkn <nafaulkn@ncsu.edu>
2017-02-02 21:59:27 -0500

    added test case for when dll's other than the SDK are used

---
+++ tests/extraLibrary/clientC#/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDK;
+using ExtraLibrary;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sample1.add(1, 2));
+            Console.WriteLine(PrimaryClass.divide(10, 2));
+        }
+    }
+}
---
+++ tests/extraLibrary/clientVB/Client/Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports SDK
+Imports ExtraLibrary
+Module Module1
+
+    Sub Main()
+        Console.WriteLine(Sample1.add(1, 2))
+        Console.WriteLine(PrimaryClass.divide(10, 2))
+    End Sub
+
+End Module
