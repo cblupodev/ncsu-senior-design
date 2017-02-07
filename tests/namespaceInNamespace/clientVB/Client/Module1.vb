1488a87a621f4d3358ab169db4428fb272cce1e4
nafaulkn <nafaulkn@ncsu.edu>
2017-02-07 17:10:34 -0500

    added a new test case for multiple name spaces

---
+++ tests/namespaceInNamespace/clientVB/Client/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports SDK
+Imports SDK.SubSpace
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine(Sample1.add(1, 2))
+        Console.WriteLine(Sample2.multiply(3, 5))
+    End Sub
+
+End Module
---
+++ tests/namespaceInNamespace/newSDK/SDK/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDK
+{
+
+    [ModelIdentifier("00000000-0000-4000-8000-00000001")]
+    public class Change1
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    namespace SubNameSpace
+    {
+
+        [ModelIdentifier("00000000-0000-4000-8000-00000002")]
+        public class Change2
+        {
+            public static int multiply(int a, int b)
+            {
+                return a * b;
+            }
+        }
+    }
+}
