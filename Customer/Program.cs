ab25e855b4bd17c147b03ca09aab4b35580c513b
pbclouse <pbclouse@ncsu.edu>
2017-03-01 21:21:00 -0500

    Add edge cases to the 'test' program

--- Customer/Program.cs
+++ Customer/Program.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DBConnector;
-using FujitsuSDKOld;
+//test
+using dsads = /* test */ FujitsuSDKOld;
 using asdf;
 
 
 namespace Customer
 {
     [ModelIdentifier("26A993C4-65C6-4168-B045-9FABB2A1526D")]
-    class Program
+    class Program:asdf.fudgeold2
     {
 
-        private FujitsuSDKOld.Sample a = new FujitsuSDKOld.Sample();
+        private dsads.Sample a = new dsads.Sample();
         //this is a comment to test keeping track of trivia
         private asdf.fudgeold2 b = new asdf.fudgeold2();
 
@@ -23,6 +24,15 @@ namespace Customer
             FujitsuSDKOld.Sample s = new FujitsuSDKOld.Sample();
             // this is also a test comment
             asdf.fudgeold2 t = new asdf.fudgeold2();
+
+            fudgeold2 cast = (fudgeold2)new Casting();
+        }
+
+        public fudgeold2 testMethod(fudgeold2 a)
+        {
+            Dictionary<String, asdf.fudgeold2> test = new Dictionary<string, fudgeold2>();
+            fudgeold2 b = new fudgeold2();
+            return b;
         }
     }
 }
