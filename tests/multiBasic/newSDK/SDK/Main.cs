3e1d3ff7e53b187e8522876bf4ab2bd425314227
nafaulkn <nafaulkn@ncsu.edu>
2017-03-16 18:39:21 -0400

    fixed tests to use the propper 8-4-4-4-12 format for model identifiers

--- tests/multiBasic/newSDK/SDK/Main.cs
+++ tests/multiBasic/newSDK/SDK/Main.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SDK.NewSpaceOne
 {
 
-    [ModelIdentifier("00000000-0000-4000-8000-00000001")]
+    [ModelIdentifier("00000000-0000-4000-8000-000000000001")]
     public class Sample1
     {
         public static int add(int a, int b)
@@ -16,7 +16,7 @@ namespace SDK.NewSpaceOne
         }
     }
 
-    [ModelIdentifier("00000000-0000-4000-8000-00000002")]
+    [ModelIdentifier("00000000-0000-4000-8000-000000000002")]
     public class Sample2
     {
         public static int add3(int a, int b, int c)
--- tests/namespaceInNamespace/newSDK/SDK/Main.cs
+++ tests/namespaceInNamespace/newSDK/SDK/Main.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SDK
 {
 
-    [ModelIdentifier("00000000-0000-4000-8000-00000001")]
+    [ModelIdentifier("00000000-0000-4000-8000-000000000001")]
     public class Change1
     {
         public static int add(int a, int b)
@@ -19,7 +19,7 @@ namespace SDK
     namespace SubNameSpace
     {
 
-        [ModelIdentifier("00000000-0000-4000-8000-00000002")]
+        [ModelIdentifier("00000000-0000-4000-8000-000000000002")]
         public class Change2
         {
             public static int multiply(int a, int b)
