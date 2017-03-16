b3c5e42dac4d46bd47127a8cd896802b0f2f837b
nafaulkn <nafaulkn@ncsu.edu>
2017-03-16 15:16:38 -0400

    fixed some incorrect black box tests

--- tests/typeof/clientVB/Client/Module1.vb
+++ tests/typeof/clientVB/Client/Module1.vb
@@ -2,8 +2,7 @@
 Module Module1
 
     Sub Main()
-        Dim obj = New Sample1()
-        Console.WriteLine(TypeOf obj Is Sample1)
+        Console.WriteLine(GetType(Sample1))
         Console.WriteLine(Sample1.add(1, 2))
     End Sub
 
--- tests/variables/clientC#/Client/Program.cs
+++ tests/variables/clientC#/Client/Program.cs
@@ -15,7 +15,7 @@ namespace Client
         {
             Console.WriteLine(Sample1.add(1, 2));
             item = new Sample1();
-            Console.WriteLine(item.mul(3, 15));
+            Console.WriteLine(item.mul(3, 5));
             Sample1 item2 = new Sample1();
             Console.WriteLine(item2.mul(4, 2));
         }
