0db3038280d5a6492f0fd94535682cd4b9e62006
nafaulkn <nafaulkn@ncsu.edu>
2017-03-23 14:48:11 -0400

    fixed test issues that surfaced now that we're testing that the mappings actually match the expected

--- tests/alias/newSDK/SDK/Main.cs
+++ tests/alias/newSDK/SDK/Main.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SDK
 {
 
-    [ModelIdentifier("00000000-0000-4000-8000-0000000000000001")]
+    [ModelIdentifier("00000000-0000-4000-8000-000000000001")]
     public class Change1
     {
         public static int add(int a, int b)
